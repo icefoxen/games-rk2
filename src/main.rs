ed65b50d8d0cb0f8bb93a6c9374abccfd86e734d
Simon Heath <icefoxen@gmail.com>
2016-11-28 20:35:05 -0500

    Tiny cleanup!

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,10 @@ use ggez::{GameResult, Context};
 use ggez::graphics;
 use ggez::timer;
 use ggez_goodies::asset;
-use ggez_goodies::asset::StateLoadable;
 
 use specs::Join;
 use nalgebra as na;
 
-use std::rc::Rc;
 use std::time::Duration;
 
 type Vec2 = na::Vector2<f64>;
@@ -39,12 +37,6 @@ impl specs::Component for CImage {
     type Storage = specs::VecStorage<CImage>;
 }
 
-// #[derive(Clone, Debug)]
-// struct CImage(graphics::Image);
-// impl specs::Component for CImage {
-// type Storage = specs::VecStorage<CImage>;
-// }
-//
 
 struct Assets<'a> {
     images: asset::AssetCache2<&'a str, graphics::Image>,
@@ -56,10 +48,8 @@ impl<'a> Assets<'a> {
     }
 }
 
-// First we make a structure to contain the game's state
 struct MainState<'a> {
     assets: Assets<'a>,
-    // world: specs::World,
     planner: specs::Planner<()>,
 }
 
@@ -83,12 +73,6 @@ fn create_player(world: &mut specs::World,
         .build()
 }
 
-// Then we implement the `ggez::game::GameState` trait on it, which
-// requires callbacks for creating the game state, updating it each
-// frame, and drawing it.
-//
-// The `GameState` trait also contains callbacks for event handling
-// that you can override if you wish, but the defaults are fine.
 impl<'a> GameState for MainState<'a> {
     fn load(ctx: &mut Context, _conf: &conf::Conf) -> GameResult<MainState<'a>> {
 
@@ -139,14 +123,6 @@ impl<'a> GameState for MainState<'a> {
     }
 }
 
-// Now our main function, which does three things:
-//
-// * First, create a new `ggez::conf::Conf`
-// object which contains configuration info on things such
-// as screen resolution and window title,
-// * Second, create a `ggez::game::Game` object which will
-// do the work of creating our MainState and running our game,
-// * then just call `game.run()` which runs the `Game` mainloop.
 pub fn main() {
     let c = conf::Conf::new();
     let mut game: Game<MainState> = Game::new("helloworld", c).unwrap();

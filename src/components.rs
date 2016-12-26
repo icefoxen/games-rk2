f3b5ae08cc0746501305fe2dc04fe253c844fb25
Simon Heath <icefoxen@gmail.com>
2016-12-25 21:56:12 -0500

    Adding background and camera and such

--- src/components.rs
+++ src/components.rs
@@ -1,5 +1,6 @@
 use specs;
 use ggez_goodies::asset;
+use ggez_goodies::camera;
 
 use util::*;
 
@@ -25,4 +26,31 @@ impl specs::Component for CPlayer {
 pub struct CImage(pub asset::AssetHandle);
 impl specs::Component for CImage {
     type Storage = specs::VecStorage<CImage>;
+}
+
+pub struct CCamera {
+    pub c: camera::Camera,
+}
+impl specs::Component for CCamera {
+    type Storage = specs::HashMapStorage<CCamera>;
+}
+
+impl CCamera {
+    pub fn new(screen_width: u32, screen_height: u32) -> Self {
+        CCamera { c: camera::Camera::new(screen_width, screen_height, 40.0, 30.0) }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct CBackgroundScroller {
+    pub scroll_speed: Vec2,
+}
+impl specs::Component for CBackgroundScroller {
+    type Storage = specs::HashMapStorage<CBackgroundScroller>;
+}
+
+impl CBackgroundScroller {
+    pub fn new() -> Self {
+        CBackgroundScroller { scroll_speed: Vec2::new(0.0, -0.01) }
+    }
 }

34559dbd24c634dc0a1d7b06f76789107b620bda
Simon Heath <icefoxen@gmail.com>
2016-12-25 20:35:52 -0500

    Builds with current version of ggez+goodies, so

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Rocket Kiwi 2: Revenge Of The Kiwi
 
 See https://wiki.alopex.li/RK2 for design doc.
+
+# TODO
+
+* Cast some extra illumination into the specs docs, 'cause getting the details right for how to roll your own single-threaded loop and stuff like that.
--- src/gamestate.rs
+++ src/gamestate.rs
@@ -10,7 +10,7 @@ use ggez::graphics;
 use ggez::timer;
 use ggez_goodies::asset;
 use ggez_goodies::input;
-use ggez_goodies::scene;
+// use ggez_goodies::scene;
 
 use specs::Join;
 
@@ -63,7 +63,7 @@ fn create_player(world: &mut specs::World,
                  assets: &mut Assets,
                  ctx: &mut Context)
                  -> specs::Entity {
-    let (handle, _) = assets.images.get_key_state(&"images/kiwi.png", ctx).unwrap();
+    let (handle, _) = assets.images.get_key_state(&"images/kiwi.png".to_string(), ctx).unwrap();
     world.create_now()
         .with(CPosition(Vec2::new(0.0, 0.0)))
         .with(CPlayer)
@@ -90,85 +90,84 @@ fn world_to_screen_coords(location: Vec2, screen_dims: (u32, u32)) -> (u32, u32)
     let y = height - (location.y + height / 2.0);
     (x as u32, y as u32)
 }
-
-struct DummySavedScene;
-struct DummyScene;
-
-impl scene::SavedScene for DummySavedScene {
-    fn load(&mut self) -> Box<scene::Scene> {
-        let mut assets = Assets::new();
-
-        let mut w = create_world();
-        // let _p = create_player(&mut w, &mut assets, ctx);
-        let planner = specs::Planner::new(w, 1);
-        let s = MainState {
-            assets: assets,
-            input: create_input_manager(),
-            planner: planner,
-            screen_dimensions: (640, 480),
-            //(conf.window_width, conf.window_height),
-        };
-    }
-    fn name(&self) -> String {
-        "test scene".to_string()
-    }
-}
-
-impl scene::Scene for DummyScene {
-    fn unload(&mut self) -> Box<scene::SavedScene> {
-        struct DummySavedScene;
-        Box::new(DummyScene)
-    }
-}
-
-impl<'a> scene::Scene for MainState<'a> {
-    fn unload(&mut self) -> Box<scene::SavedScene> {
-        Box::new(DummyScene)
-    }
-
-
-
-    fn update(&mut self, _ctx: &mut Context, dt: Duration) -> GameResult<()> {
-        let seconds = timer::duration_to_f64(dt);
-        self.input.update(seconds);
-        let x_axis = self.input.get_axis(Axis::Horz);
-        let y_axis = self.input.get_axis(Axis::Vert);
-
-        let player_update = move |pos: &mut CPosition| {
-            let xvel = 100.0 * x_axis * seconds;
-            let yvel = 100.0 * y_axis * seconds;
-            pos.0 += Vec2::new(xvel, yvel);
-        };
-        self.planner.run1w0r(player_update);
-        Ok(())
-    }
-
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        ctx.renderer.clear();
-
-        let world = self.planner.mut_world();
-        let positions = world.read::<CPosition>();
-        let playermarkers = world.read::<CPlayer>();
-        let images = world.read::<CImage>();
-
-        for (pos, _player, image) in (&positions, &playermarkers, &images).iter() {
-            // println!("Position is: {:?}, {:?}, {:?}", pos, player, image);
-            let kiwi = self.assets.images.get_mut(image.0).unwrap();
-            let w = kiwi.width();
-            let h = kiwi.height();
-            let (screen_x, screen_y) = world_to_screen_coords(pos.0, self.screen_dimensions);
-            let r = graphics::Rect::new(screen_x as i32, screen_y as i32, w, h);
-            graphics::draw(ctx, kiwi, None, Some(r))?;
-        }
-
-        ctx.renderer.present();
-        timer::sleep_until_next_frame(ctx, 60);
-        Ok(())
-    }
-}
-
-impl<'a> GameState for MainState<'a> {
-    fn load(ctx: &mut Context, conf: &conf::Conf) -> GameResult<MainState<'a>> {
+// struct DummySavedScene;
+// struct DummyScene;
+//
+// impl scene::SavedScene for DummySavedScene {
+// fn load(&mut self) -> Box<scene::Scene> {
+// let mut assets = Assets::new();
+//
+// let mut w = create_world();
+// let _p = create_player(&mut w, &mut assets, ctx);
+// let planner = specs::Planner::new(w, 1);
+// let s = MainState {
+// assets: assets,
+// input: create_input_manager(),
+// planner: planner,
+// screen_dimensions: (640, 480),
+// conf.window_width, conf.window_height),
+// };
+// }
+// fn name(&self) -> String {
+// "test scene".to_string()
+// }
+// }
+//
+// impl scene::Scene for DummyScene {
+// fn unload(&mut self) -> Box<scene::SavedScene> {
+// struct DummySavedScene;
+// Box::new(DummyScene)
+// }
+// }
+//
+// impl<'a> scene::Scene for MainState<'a> {
+// fn unload(&mut self) -> Box<scene::SavedScene> {
+// Box::new(DummyScene)
+// }
+//
+//
+//
+// fn update(&mut self, _ctx: &mut Context, dt: Duration) -> GameResult<()> {
+// let seconds = timer::duration_to_f64(dt);
+// self.input.update(seconds);
+// let x_axis = self.input.get_axis(Axis::Horz);
+// let y_axis = self.input.get_axis(Axis::Vert);
+//
+// let player_update = move |pos: &mut CPosition| {
+// let xvel = 100.0 * x_axis * seconds;
+// let yvel = 100.0 * y_axis * seconds;
+// pos.0 += Vec2::new(xvel, yvel);
+// };
+// self.planner.run1w0r(player_update);
+// Ok(())
+// }
+//
+// fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+// ctx.renderer.clear();
+//
+// let world = self.planner.mut_world();
+// let positions = world.read::<CPosition>();
+// let playermarkers = world.read::<CPlayer>();
+// let images = world.read::<CImage>();
+//
+// for (pos, _player, image) in (&positions, &playermarkers, &images).iter() {
+// println!("Position is: {:?}, {:?}, {:?}", pos, player, image);
+// let kiwi = self.assets.images.get_mut(image.0).unwrap();
+// let w = kiwi.width();
+// let h = kiwi.height();
+// let (screen_x, screen_y) = world_to_screen_coords(pos.0, self.screen_dimensions);
+// let r = graphics::Rect::new(screen_x as i32, screen_y as i32, w, h);
+// graphics::draw(ctx, kiwi, None, Some(r))?;
+// }
+//
+// ctx.renderer.present();
+// timer::sleep_until_next_frame(ctx, 60);
+// Ok(())
+// }
+// }
+//
+impl<'a> GameState for MainState {
+    fn load(ctx: &mut Context, conf: &conf::Conf) -> GameResult<Self> {
 
         let mut assets = Assets::new();
 
@@ -180,7 +179,6 @@ impl<'a> GameState for MainState<'a> {
             input: create_input_manager(),
             planner: planner,
             screen_dimensions: (conf.window_width, conf.window_height),
-            scene_mgr: scene::SceneManager::new(DummyScene),
         };
         Ok(s)
     }
